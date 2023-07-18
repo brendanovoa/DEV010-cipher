1855e39c79ebd57ae17dc44386d235aa652afc62
Brenda Hernandez <brendanovoabn@gmail.com>
2023-07-18 10:32:45 -0600

    Reordenar estructura de html, inicio css y js

--- src/cipher.js
+++ src/cipher.js
@@ -2,4 +2,27 @@ const cipher = {
   // ...
 };
 
+const textToCode = document.getElementById("textToCode")
+const textToDecode = document.getElementById("textToDecode")
+const offest = document.getElementById("offest")
+const offsetDecode = document.getElementById("offestDecode")
+const btnCode = document.getElementById("code")
+const btnDecode = document.getElementById("decode")
+const resultado = document.getElementById("resultado")
+
+btnCode.addEventListener('click', cipher)
+
+function cipher(){
+  let inputValue = textToCode.value;
+  let offsetValue = offset.value;
+
+  let resultado = ;
+  
+  // (("valor ascii" - 65 + offset) / 26 + 65)
+  // Texto.charCodeAt(0);
+  // String.fromCharCode(76);
+
+    window.alert("resultado");
+}
+
 export default cipher;
