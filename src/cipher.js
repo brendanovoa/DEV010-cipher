47f3c0f626938251df1fbc6f980be19b2a3772b2
Brenda Hernandez <brendanovoabn@gmail.com>
2023-07-26 09:25:39 -0600

    Función de encode funcionando y arreglos en css

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,64 @@
+/* 
+Implementar el objeto cipher que contiene 2 métodos o funciones:
+cipher.encode(offset, string)
+cipher.decode(offset, string)
+*/
+
+const textToCode = document.getElementById("textToCode");
+const textToDecode = document.getElementById("textToDecode");
+const offsetCode = document.getElementById("offset");
+const offsetDecode = document.getElementById("offestDecode");
+
+const textCoded = document.getElementById("textCoded");
+const textDecoded = document.getElementById("textDecoded");
+
 const cipher = {
-  // ...
-};
-
-const textToCode = document.getElementById("textToCode")
-const textToDecode = document.getElementById("textToDecode")
-const offest = document.getElementById("offest")
-const offsetDecode = document.getElementById("offestDecode")
-const btnCode = document.getElementById("code")
-const btnDecode = document.getElementById("decode")
-const resultado = document.getElementById("resultado")
-
-btnCode.addEventListener('click', cipher)
-
-function cipher(){
-  let inputValue = textToCode.value;
-  let offsetValue = offset.value;
-
-  let resultado = ;
+  encode: function () { // parámetros (offset,string)
+    const offsetValue = offsetCode.value; //convertir objeto en valor
+    const inputValue = textToCode.value;
+    console.log (offsetValue,inputValue); //verificar lo que captura
+    
+    let password = ""; // regresar valor vacio
   
-  // (("valor ascii" - 65 + offset) / 26 + 65)
-  // Texto.charCodeAt(0);
-  // String.fromCharCode(76);
-
-    window.alert("resultado");
-}
+    for (let i = 0; i < inputValue.length; i++) {
+      const character = inputValue[i];
+      const unicodeValue = inputValue.charCodeAt(i); // convertir a ascii
+      console.log (unicodeValue);
+      if (unicodeValue >= 65 && unicodeValue <= 90){ // código ASCII de letras mayúsculas
+        const charnum = ((unicodeValue - 65 + parseInt(offsetValue)) % 26 + 65); // parseInt convertir string a número entero
+        const character = String.fromCharCode(charnum);
+        password += character;
+      } 
+      else{
+        alert ("El texto ingresado no es válido");
+      }
+      textCoded.value = password; // mostrar resultado en el cuadro  de respuesta
+      console.log (password); 
+    }
+  },
+  decode: function () {
+    const offsetValue = offsetDecode.value; //convertir objeto en valor
+    const inputValue = textToDecode.value;
+    console.log (offsetValue,inputValue); //verificar lo que captura
+  
+    let password = ""; // regresar valor vacio
+  
+    for (let i = 0; i < inputValue.length; i++) {
+      const character = inputValue[i];
+      const unicodeValue = inputValue.charCodeAt(i); // convertir a ascii
+      console.log (unicodeValue);
+      if (unicodeValue >= 65 && unicodeValue <= 90){ // código ASCII de letras mayúsculas
+        const charnum = (((unicodeValue - 65) - parseInt(offsetValue)) % 26 + 65); // parseInt convertir string a número entero
+        const character = String.fromCharCode(charnum);
+        password += character;
+      } 
+      else{
+        alert ("El texto ingresado no es válido");
+      }
+      textDecoded.value = password; // mostrar resultado en el cuadro  de respuesta
+      console.log (password); 
+    }
+  }
+};
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+// Acá debes escuchar eventos del DOM, invocar cipher.encode() o cipher.decode() según sea necesario y actualizar el resultado en la UI.
+
+
+const btnCode = document.getElementById("btn-code");
+const btnDecode = document.getElementById("btn-decode");
+
 import cipher from './cipher.js';
 
+btnCode.addEventListener('click', cipher.encode);
+btnDecode.addEventListener('click', cipher.decode);
+
 console.log(cipher);
+
+
+
+// let string = JSON.stringify(textToCode);  // método para convertir un objeto en una cadena de texto (string) para poder manipularlo o almacenarlo en algún formato de texto
+// console.log (typeof offsetValue); // saber qué tipo de elemento es
+// charAt método utilizado en cadenas de texto para obtener el carácter en una posición específica. Devuelve el carácter ubicado en el índice proporcionado como argumento.
+// const character = inputValue.charAt(i) ; // separar caracteres
+// resultado += String.fromCharCode(unicodeValue); // unir caracteres en texto
+// inputValue.split('') // separar caracteres
+// const caracter = inputValue.charAt(i); // separar caracteres
+// password = String.fromCharCode(resultados)
+// return password;
+//const character = String.fromCharCode(((unicodeValue - 65 + offsetValue) % 26) + 65); // (("valor ascii" - 65 + offset) / 26 + 65)
+//const offsetValue = parseInt(offsetValue); // convertir string a número entero
+
+/*  for (let i = 0; i < inputValue.length; i++) {
+      const character = inputValue[i];
+      const unicodeValue = inputValue.charCodeAt(i); // convertir a ascii
+      const charCode = unicodeValue + offset.value;
+      if (charCode >= 65 && charCode <= 90){ // código ASCII de letras mayúsculas
+        const character = String.fromCharCode(unicodeValue + offsetValue); 
+        password += character;
+      }
+      else if (charCode > 90 && charCode <= 116){
+        const character = String.fromCharCode(((unicodeValue + offsetValue)-90)+64); 
+        password += character;
+      } */
